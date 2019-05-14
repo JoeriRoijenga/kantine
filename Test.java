4e8ec221c09808c2e8870ea40d95f1989f2240bf
joeriroijenga <roijenga.joeri@gmail.com>
2019-05-14 09:53:35 +0200

    Persoon & Artikel

--- Test.java
+++ Test.java
@@ -1,8 +1,11 @@
 public class Test {
-    private Persoon persoon;
-
     public static void main(String[] args) {
-        Persoon persoon = new Persoon(748372947, "Test", "Tester", "13-05-2019", "M");
-        System.out.println(persoon.getGeslacht());
+        Persoon persoon = new Persoon(748372947, "Test", "Tester", "13-05-2019", "V");
+        System.out.println(persoon.toString());
+        Persoon persoon1 = new Persoon(748372948, "Test", "Tester");
+        System.out.println(persoon1.toString());
+
+        Artikel artikel = new Artikel("Banaan", 1.99);
+        System.out.println(artikel.toString());
     }
 }
